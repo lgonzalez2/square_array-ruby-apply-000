3aa0ac175413ed68eb88477b9570228937dfa5a7
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-05-19 23:58:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,14 +2,9 @@ numbers = [1, 2, 3]
 
 def square_array(numbers)
   
-  numbers.each do |num|
-    num ** 2 
-    
-    square_numbers << num 
+  numbers.each do |i|
+    i ** 2 
     
   end
-  
-  square_numbers
-  
 end
 
