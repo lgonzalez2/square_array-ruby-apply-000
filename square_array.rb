e0427d8527c3b1555214811e5d2e84733fab4f33
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-05-20 00:13:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,11 @@
-numbers = [1,2,3]
+numbers = [1, 2, 3]
+
+def square_array(numbers)
+  square_numbers = []
   
   numbers.each do |i|
     square = i ** 2 
     square_numbers << square
   end
-square_numbers
+  square_numbers
 end
-
